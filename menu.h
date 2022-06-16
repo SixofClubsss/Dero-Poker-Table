9dfaac67e4276d76dc6c5b7246d2316b91999296
SixofClubs <84689659+SixofClubsss@users.noreply.github.com>
2022-06-15 21:51:00 -0600

    Commit menu update
    Patch for v1.0.1

---
+++ menu.h
@@ -0,0 +1,81 @@
+#ifndef MENU_H
+#define MENU_H
+/// dReam Tables Five Card Draw Poker
+#include "rpc/rpc.h"
+#include "aes/qaesencryption.h"
+#include "QDialog"
+#include "QFile"
+
+
+namespace Ui { class Menu; }
+
+class Menu : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Menu(QWidget *parent = nullptr);
+    ~Menu();
+    static QString userInfo;
+    static QString passInfo;
+    static QString contractAddress;
+    static QString listingAddress;
+    static QString donationAddress;
+    static bool daemonConnected;
+    static bool walletConnected;
+    static bool mainnet;
+    static bool dReams;
+    static bool afPayout;
+    void setFonts();
+    void loginInfo();
+    void delay();
+    void unlockTable();
+    int getDreams();
+    int checkDaemon();
+    int checkWallet();
+    int checkContract();
+    int checkAddress();
+    int fetchInfo();
+    int setTable();
+    int cleanTable();
+    int fetchListingScData();
+    int checkIfListed();
+    int listTable();
+    int delistTable();
+
+private slots:
+    void daemonToggle();
+
+    void walletToggle();
+
+    void contractToggle();
+
+    void confirmationBox();
+
+    void on_contractButton_clicked();
+
+    void on_daemonRPCbutton_clicked();
+
+    void on_walletRPCbutton_clicked();
+
+    void on_setTableButton_clicked();
+
+    void on_cleanTableButton_clicked();
+
+    void on_getTableButton_clicked();
+
+    void on_newTableButton_clicked();
+
+    void on_autoFoldPayRButton_clicked();
+
+    void on_listTableButton_clicked();
+
+    void on_delistTableButton_clicked();
+
+    void on_findTablesButton_clicked();
+
+private:
+    Ui::Menu *ui;
+};
+
+#endif // MENU_H
